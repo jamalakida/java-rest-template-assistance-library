79445f229d7dd4371623087af3ff7afd848867d4
JAMAL_AKIDA_AJUAYE <jamal.ajuaye@pccb.go.tz>
2023-10-24 15:33:17 +0300

    Implemented annotation calling

---
+++ src/main/java/rest/template/library/bean/RemoteAssistanceAutoConfiguration.java
@@ -0,0 +1,9 @@
+package rest.template.library.bean;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("rest.template.library")
+public class RemoteAssistanceAutoConfiguration {
+}
